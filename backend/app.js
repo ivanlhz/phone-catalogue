265f7aea3347fca9bf6d05afc78d484f13ce7084
ivanlhz <ivanlhz@gmail.com>
2020-07-31 06:41:54 +0100

    Allow CORS

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+app.use(cors())
 
 app.get('/phones', function (req, res) {
   fs.readFile(path.join(__dirname,'phones.json'), 'utf8', function (err, data) {
