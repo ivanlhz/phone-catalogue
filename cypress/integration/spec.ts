13da4717be685569a4fc7d725dd6e76f252bf952
ivanlhz <ivanlhz@gmail.com>
2020-08-02 08:43:34 +0100

    Fix wait cypress

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -2,7 +2,8 @@ describe('Phone Catalogue', () => {
   it('shows homepage and go to the first phone link', function () {
     cy.visit('http://localhost:3000');
     cy.get('[role="spinbutton"]')
-    cy.get('[href="/phones/0"] > .block > h3').click(); 
+    cy.wait(4000)
+    cy.get('[href="/phones/0"]').click(); 
     cy.get('ul > :nth-child(2)').should('contain', 'A10 Fusion')
   })
 })
